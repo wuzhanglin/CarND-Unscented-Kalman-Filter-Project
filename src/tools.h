716350c966fbfa0d312f045cbd4d464f41c9daed
wuzhanglin <jason.lin.wu@163.com>
2018-09-30 11:29:08 +0800

    Safe coding style fixes
    Re-indent the code to make it look nicer.

--- src/tools.h
+++ src/tools.h
@@ -1,5 +1,6 @@
 #ifndef TOOLS_H_
 #define TOOLS_H_
+
 #include <vector>
 #include "Eigen/Dense"
 
@@ -7,23 +8,25 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using namespace std;
 
-class Tools {
+class Tools
+{
 public:
-  /**
-  * Constructor.
-  */
-  Tools();
-
-  /**
-  * Destructor.
-  */
-  virtual ~Tools();
-
-  /**
-  * A helper method to calculate RMSE.
-  */
-  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
-
+    
+    /**
+     * Constructor
+     */
+    Tools();
+    
+    /**
+     * Destructor
+     */
+    virtual ~Tools();
+    
+    /**
+     * A helper method to calculate RMSE
+     */
+    VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+    
 };
 
 #endif /* TOOLS_H_ */
